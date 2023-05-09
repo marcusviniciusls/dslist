4412ccfe0622dfd0b2969ba81c9fe745e4c4e329
Marcus Vinicius Lima Silva <marcus.silva.dev@gmail.com>
2023-05-09 15:18:00 -0300

    Game seed

--- src/main/java/com/programmingschool/dslist/entities/Game.java
+++ src/main/java/com/programmingschool/dslist/entities/Game.java
@@ -1,2 +1,35 @@
-package com.programmingschool.dslist.entities;public class Game {
+package com.programmingschool.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    
+    private String genre;
+    private String platforms;
+    private BigDecimal score;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
 }
